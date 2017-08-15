255b784c727843cf6496d52571b6885323feebf5
Paul Walsh <twentytwentyk@live.com>
2017-08-15 16:15:20 -0400

    rows expand

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,6 +5,15 @@ import { CardSection } from './common';
 import * as actions from '../actions';
 
 class ListItem extends Component {
+  renderDescription() {
+    const { library, selectedLibraryId } = this.props;
+    if (library.id === selectedLibraryId) {
+      return (
+        <Text>{library.description}</Text>
+      );
+    }
+  };
+
   render () {
     const { titleStyle } = styles;
     const { id, title } = this.props.library; //id + title from libraryList.json
@@ -19,6 +28,7 @@ class ListItem extends Component {
               {title}
             </Text>
           </CardSection>
+          {this.renderDescription()}
         </View>
       </TouchableWithoutFeedback>
     );
@@ -32,7 +42,11 @@ const styles = {
   },
 };
 
-export default connect(null, actions)(ListItem);
+const mapStateToProps = state => {
+  return { selectedLibraryId: state.selectedLibraryId };
+};
+
+export default connect(mapStateToProps, actions)(ListItem);
 
 //^mapStateToProps must be the first arguement in connect()
 //null is there because we don't have mapStateToProps
--- src/reducers/SelectionReducer.js
+++ src/reducers/SelectionReducer.js
@@ -1,6 +1,10 @@
-export default (state, action) => {
-  console.log(action);
-  return null;
+export default (state = null, action) => { //if state is undefined default to null
+  switch (action.type) {
+    case 'select_library':
+      return action.payload; //record selected libary id
+    default:
+      return state; //orginal state default
+  }
 };
 
 //reducers use state and action as arguements
