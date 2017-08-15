cfdc1e4c9ffd91812e8c5dced1e8da5b40ad625c
Paul Walsh <twentytwentyk@live.com>
2017-08-15 13:37:04 -0400

    selection reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,15 @@
 import { combineReducers } from 'redux';
 import LibraryReducer from './LibraryReducer';
+import SelectionReducer from './SelectionReducer';
 
 //one reducer one file
 
 export default combineReducers({
   libraries: LibraryReducer,
+  selectedLibraryId: SelectionReducer,
 });
 
 //when our app first boots up it evaluates this statement
 //our state object has a key of 'libraries'
 //a value of the list of libraries produced in the reducer
+//selectedLibraryId indicates the selected library.
